00d1e6c254c85135499ca6d5bbe4ef5a96c0d2a6
RaniNamala <namalarani161@gmail.com>
2025-01-07 22:26:52 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,9 +1,17 @@
-
-
+import Home from './Home'
+import Contact from './Contact'
+import About from './About'
+import Greeting from './Greeting'
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+    <h1>Hello from ReactJS</h1>
+    <Greeting name = {'Macha'}/>
+    <Greeting name = {'Nani'} />
+    {/*<Home name = {'Pooja'} age = {24} />
+    <About />
+    <Contact /> */}
+    </div>
   );    
 }
-
-export default App;
+export default App; 
---
+++ basics/src/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home(Props) {
+    return (
+        <div>Welcome {Props.name}
+        {Props.age}
+        </div>
+    )
+}
